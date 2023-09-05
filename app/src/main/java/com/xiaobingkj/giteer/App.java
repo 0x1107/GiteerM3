d81b8e20355c8243c7bf849295f285e6807ec4ac
Jane Thompson <339002469@qq.com>
2023-09-05 21:56:51 +0800

    NoActionBar

---
+++ app/src/main/java/com/xiaobingkj/giteer/App.java
@@ -0,0 +1,20 @@
+package com.xiaobingkj.giteer;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
